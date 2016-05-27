fdc7700b649281cb8dd5f6cb7705eaeaa1b3a926
Ahmed Shehab Khan <the.shihab.khan@gmail.com>
2016-05-27 12:05:31 -0400

    modeltrain.sh changed a little

--- examples/motionFeatureFusion/modeltrain.sh
+++ examples/motionFeatureFusion/modeltrain.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-./build/tools/caffe train -gpu 0 -solver=examples/motionFeatureFusion/Demo/solver.prototxt
+./build/tools/caffe train -gpu 0 -solver=examples/motionFeatureFusion/fusion/solver_link_from_appearance.prototxt
